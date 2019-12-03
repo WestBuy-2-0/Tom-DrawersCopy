6942d76f1d2cb6f74ffc1b360ccdcfbb170fd8e0
Collin Snyder <collinjacobsnyder@gmail.com>
2019-12-03 13:10:49 -0600

    Introduce server structure and initial components

---
+++ client/src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h2>A chest of drawers will live here.</h2>;
+  }
+}
