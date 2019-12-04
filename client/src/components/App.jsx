68415c5cba2b41ac5382aa698541df35011e2e05
Collin Snyder <collinjacobsnyder@gmail.com>
2019-12-03 18:50:29 -0600

    Add initial font, border, and caret styling to drawers

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,9 +5,13 @@ import Drawers from "./Drawers.jsx";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      productId: 1
+    }
   }
 
   render() {
-    return <Drawers />;
+    return <Drawers productId={this.state.productId} />;
   }
 }
---
+++ client/src/components/QandA.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Collapse from "react-bootstrap/Collapse";
+
+export default class QandA extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState(state => {
+      return { open: !state.open };
+    });
+  }
+
+  render() {
+    return (
+      <Card>
+        <Card.Header onClick={this.toggle}>
+          <span className="drawer-label">Question & Answer</span>
+          <span className="drawer-caret">
+            <svg viewBox="0 0 32 32">
+              <path d="M31.7858,11.08051c0.35494,0.4731,0.25868,1.15101-0.21397,1.50645L16.81907,23.67027 c-0.47265,0.35519-1.24607,0.35424-1.71803-0.00219L0.42612,12.58502c-0.47191-0.35643-0.56652-1.03429-0.21009-1.5065 L2.1675,8.49396C2.52393,8.02181,3.20179,7.92744,3.674,8.28388l11.43631,8.63729c0.47216,0.35648,1.24532,0.35743,1.71803,0.00219 l11.50777-8.6448c0.47265-0.35549,1.15056-0.25918,1.50555,0.21397L31.7858,11.08051z"></path>
+              <path d="M31.7858,20.91949c0.35494-0.4731,0.25868-1.15101-0.21397-1.50645L16.81907,8.32973 c-0.47265-0.35519-1.24607-0.35424-1.71803,0.00219L0.42612,19.41498c-0.47191,0.35643-0.56652,1.03429-0.21009,1.5065 l1.95147,2.58456c0.35643,0.47216,1.03429,0.56652,1.5065,0.21008l11.43631-8.63729 c0.47216-0.35648,1.24532-0.35743,1.71803-0.00219l11.50777,8.64481c0.47265,0.35549,1.15056,0.25918,1.50555-0.21397 L31.7858,20.91949z"></path>
+            </svg>
+          </span>
+        </Card.Header>
+        <Collapse in={this.state.open}>
+          <Card.Body>This is where the product Q&A will go.</Card.Body>
+        </Collapse>
+      </Card>
+    );
+  }
+}
