dcae1cb3af0f0f0e2e42c26b53649d3dfff0ac16
Collin Snyder <collinjacobsnyder@gmail.com>
2019-12-05 20:36:33 -0600

    Style review filter checkboxes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -15,6 +15,7 @@ export default class App extends React.Component {
     return (
       <div className="container">
         <Drawers productId={this.state.productId} />
+        <div className="icon-credit">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
       </div>
     );
   }
--- client/src/components/reviews/RatingBar.jsx
+++ client/src/components/reviews/RatingBar.jsx
@@ -5,44 +5,57 @@ import "./RatingBar.css";
 import { thisExpression } from "@babel/types";
 
 export default class RatingBar extends React.Component {
-    constructor (props) {
-        super (props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            checked: false
-        };
+    this.state = {
+      checked: false
+    };
 
-        this.progressBarPct = `${((this.props.ratingCount / this.props.totalReviews) * 100)}%`;
-        this.handleChange = this.handleChange.bind(this);
-    }
+    this.progressBarPct = `${(this.props.ratingCount /
+      this.props.totalReviews) *
+      100}%`;
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange () {
-        this.setState(state => {
-            return {checked: !state.checked};
-        })
-    }
+  handleChange() {
+    this.setState(state => {
+      return { checked: !state.checked };
+    });
+  }
 
-    render () {
-        return (
-            <div className="rating-bar-container">
-                <div className="checkbox-container">
-                    <input type="checkbox" id={this.props.starRating + "-checkbox"} name={this.props.starRating + "-checkbox"} checked={this.state.checked} onChange={this.handleChange} ></input>
-                </div>
-                <div className="star-rating">
-                    <span className="star">{this.props.starRating}</span>
-                    <i className="gold-star"></i>
-                </div>
-                <div className="progress-bar">
-                    <span className="progress-bar-filled" style={{width: this.progressBarPct}}></span>
-                </div>
-                <div className="rating-count">{this.props.ratingCount}</div>
-            </div>
-        )
-    }
-};
+  render() {
+    return (
+      <div className="rating-bar-container">
+        <div className="checkbox-container">
+          <input
+            type="checkbox"
+            id={this.props.starRating + "-checkbox"}
+            name={this.props.starRating + "-checkbox"}
+            checked={this.state.checked}
+            onChange={this.handleChange}
+          ></input>
+          <i className="fancy-checkbox"></i>
+          <div className="fancy-checkbox"></div>
+        </div>
+        <div className="star-rating">
+          <span className="star">{this.props.starRating}</span>
+          <i className="gold-star"></i>
+        </div>
+        <div className="progress-bar">
+          <span
+            className="progress-bar-filled"
+            style={{ width: this.progressBarPct }}
+          ></span>
+        </div>
+        <div className="rating-count">{this.props.ratingCount}</div>
+      </div>
+    );
+  }
+}
 
 RatingBar.propTypes = {
-    starRating: PropTypes.number,
-    ratingCount: PropTypes.number,
-    totalReviews: PropTypes.number
-}
+  starRating: PropTypes.number,
+  ratingCount: PropTypes.number,
+  totalReviews: PropTypes.number
+};
