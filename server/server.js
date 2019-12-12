4fd78a120440543e0e1f3a41364085a120bb4d00
Collin Snyder <collinjacobsnyder@gmail.com>
2019-12-12 12:12:09 -0600

    Initialize three initial get routes for data

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const writeReviews = require("./dataGenerators/reviewDataGenerator.js");
 const writeOverview = require("./dataGenerators/overviewDataGenerator.js");
 const writeSpecs = require("./dataGenerators/specsDataGenerator.js");
+const axios = require("axios");
 const port = 3333;
 
 // writeReviews.writeReviews();
@@ -12,6 +13,18 @@ const app = express();
 
 app.use(express.static(__dirname + "/../client/dist"));
 app.use(express.json());
+
+app.get('/overview', (req, res) => {
+  res.send("Received your get request for overviews");
+});
+
+app.get('/features', (req, res) => {
+  res.send("Received your get request for features");
+});
+
+app.get('/reviews', (req, res) => {
+  res.send("Received your get request for reviews");
+});
 
 app.listen(port, () => {
   console.log("Now listening on port " + port + "!");
