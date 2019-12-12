2b681c15998bb7f58962a7f82e7b8ba6072fa1da
Collin Snyder <collinjacobsnyder@gmail.com>
2019-12-12 16:38:20 -0600

    Update db config

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,8 @@
 const { Pool } = require("pg");
-
 const types = require("pg").types;
+const dbConfig = require("../../env/db.config");
 
-const pool = new Pool({
-    user: 'postgres',
-    host: 'west-buy-drawers.cuigt8x5y4kt.us-east-2.rds.amazonaws.com',
-    database: 'drawers',
-    password: 'js9dH-usHHS8-Jh',
-    port: 5432
-});
+const pool = new Pool(dbConfig);
 
 types.setTypeParser(20, (val) => parseInt(val));
 types.setTypeParser(23, (val) => parseInt(val));
