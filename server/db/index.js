3c5fb7f2d1e9766f047d3d02e6a907a3145b31f0
Collin Snyder <collinjacobsnyder@gmail.com>
2019-12-13 20:06:25 -0600

    Refactor DB query result type parsing to output floats instead of ints

--- server/db/index.js
+++ server/db/index.js
@@ -4,9 +4,9 @@ const dbConfig = require("../../env/db.config");
 
 const pool = new Pool(dbConfig);
 
-types.setTypeParser(20, (val) => parseInt(val));
-types.setTypeParser(23, (val) => parseInt(val));
-types.setTypeParser(1700, (val) => parseInt(val));
+types.setTypeParser(20, (val) => parseFloat(val));
+types.setTypeParser(23, (val) => parseFloat(val));
+types.setTypeParser(1700, (val) => parseFloat(val));
 
 module.exports = {
   query: (qStr, qParams) => pool.query(qStr, qParams)
