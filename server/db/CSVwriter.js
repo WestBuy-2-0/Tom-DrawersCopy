2cec53cc985c1ab29a1711cc7764e4698263a909
Tom Chandler <chandlertom71@gmail.com>
2019-12-23 11:06:08 -0600

    working on separating CSV writing by section

--- server/db/CSVwriter.js
+++ server/db/CSVwriter.js
@@ -6,56 +6,30 @@ const combinedGenerator = require('./combinedGenerator');
 let overviewCSV = 'product_id, description, features, whats_included\n';
 let specsCSV = 'product_id, key_specs, general, warranty, other\n';
 let reviewMetricSummariesCSV = 'product_id, count, summary/review_count, summary/average_rating, summary/five_star, summary/four_star, summary/three_star, summary/two_star, summary/one_star, summary/would_recommend_pct\n';
-let reviewsOnlyCSV = 'product_id, submitter, submission_date, rating, title, text, verified_purchase, would_recommend, helpful_count, unhelpful_count, rated_features, quality_rating, value_rating, ease_of_use_rating\n';
+let reviewsOnlyCSV = `product_id, submitter, submission_date, rating, title, text, verified_purchase, would_recommend, helpful_count, unhelpful_count, rated_features, quality_rating, value_rating, ease_of_use_rating\n`;
 
 let random = 3;
-let count = 125000;
+let target = 100;
+let batchSize = 10;
+let lastIndex = 0;
 
-const writeCSV = (amount) => {
-  for (let i = 1; i <= amount; i++) {
-    const newProduct = combinedGenerator(i, random++);
+const writeOverviewCSV = (last, batch) => {
+  console.time('Write time');
+  if (lastIndex === target) {
+    setTimeout(() => console.log('Completed generating overviews.'), 2000);
+    return;
+  }
+
+  const stop, lastIndex = last + batch;
+
+  for (let i = last + 1; i <= stop; i++) {
+    let overview = combinedGenerator.createOverview(i, random++);
 
     overviewCSV += `${i},${
-      newProduct.overview.description},${JSON.stringify(
-      newProduct.overview.features)},${JSON.stringify(
-      newProduct.overview.whats_included)}\n`;
-
-    specsCSV += `${i},${JSON.stringify(
-      newProduct.specData.key_specs)},${JSON.stringify(
-      newProduct.specData.general)},${JSON.stringify(
-      newProduct.specData.warranty)},${JSON.stringify(
-      newProduct.specData.other)}\n`;
-
-    reviewMetricSummariesCSV += `${i},${
-      newProduct.reviewData.count},${
-      newProduct.reviewData.reviewSummaryData.review_count},${
-      newProduct.reviewData.reviewSummaryData.average_rating},${
-      newProduct.reviewData.reviewSummaryData.five_star},${
-      newProduct.reviewData.reviewSummaryData.four_star},${
-      newProduct.reviewData.reviewSummaryData.three_star},${
-      newProduct.reviewData.reviewSummaryData.two_star},${
-      newProduct.reviewData.reviewSummaryData.one_star},${
-      newProduct.reviewData.reviewSummaryData.would_recommend_pct}\n`;
-
-    if (newProduct.reviewData.reviews) {
-      for(let z = 0; z < newProduct.reviewData.reviews.length; z++) {
-        reviewsOnlyCSV += `${i},${
-          newProduct.reviewData.reviews[z].submitter},${
-          newProduct.reviewData.reviews[z].submission_date},${
-          newProduct.reviewData.reviews[z].rating},${
-          newProduct.reviewData.reviews[z].title},${
-          newProduct.reviewData.reviews[z].text},${
-          newProduct.reviewData.reviews[z].verified_purchase},${
-          newProduct.reviewData.reviews[z].would_recommend},${
-          newProduct.reviewData.reviews[z].helpful_count},${
-          newProduct.reviewData.reviews[z].unhelpful_count},${
-          newProduct.reviewData.reviews[z].rated_features},${
-          newProduct.reviewData.reviews[z].quality_rating},${
-          newProduct.reviewData.reviews[z].value_rating},${
-          newProduct.reviewData.reviews[z].ease_of_use_rating}\n`;
-      }
-    }
-  }
+      overview.description},${JSON.stringify(
+      overview.features)},${JSON.stringify(
+      overview.whats_included)}\n`;
+  };
 
   fs.writeFile(
     path.join(__dirname, 'data/csv/overviews/overviewsData.csv'),
@@ -63,9 +37,33 @@ const writeCSV = (amount) => {
     (err) => {
       if (err) throw err;
 
-      console.log('Wrote overviews csv.');
+      console.log(`Wrote overviews CSV batch. Current total is ${stop} out of ${target}`);
+      console.timeEnd('Write time');
     }
   );
+}
+
+const writeSpecsCSV = (last, batch) => {
+  if (lastIndex === target) {
+    setTimeout(() => console.log('Completed generating overviews.'), 2000);
+    return;
+  }
+
+  const stop, lastIndex = last + batch;
+
+  for (let i = last + 1; i <= last + batch; i++) {
+    let specsData = combinedGenerator.createSpecs(i, random++);
+
+    specsCSV += `${i},${
+      JSON.stringify(specsData.key_specs)
+    },${
+      JSON.stringify(specsData.general)
+    },${
+      JSON.stringify(specsData.warranty)
+    },${
+      JSON.stringify(specsData.other)
+    }\n`
+  }
 
   fs.writeFile(
     path.join(__dirname, 'data/csv/specs/specsData.csv'),
@@ -73,9 +71,38 @@ const writeCSV = (amount) => {
     (err) => {
       if (err) throw err;
 
-      console.log('Wrote specs csv.');
+      console.log(`Wrote specs CSV batch.  Current total is ${stop} out of ${target}.`);
+      console.timeEnd('Write time');
     }
   );
+}
+
+
+
+const writeReviewMetricsCSV = (last, batch) => {  
+  if (lastIndex === target) {
+    setTimeout(() => console.log('Completed generating overviews.'), 2000);
+    return;
+  }
+
+  const stop, lastIndex = last + batch;
+
+  for (let i = last + 1; i <= last + batchSize; i++) {
+    let metrics = combinedGenerator.createReviews(product_id, random);
+
+    reviewMetricSummariesCSV += `${i},${
+      metrics.count},${
+      metrics.reviewSummaryData.review_count},${
+      metrics.reviewSummaryData.average_rating},${
+      metrics.reviewSummaryData.five_star},${
+      metrics.reviewSummaryData.four_star},${
+      metrics.reviewSummaryData.three_star},${
+      metrics.reviewSummaryData.two_star},${
+      metrics.reviewSummaryData.one_star},${
+      metrics.reviewSummaryData.would_recommend_pct}\n`;
+
+    random++;
+  }
 
   fs.writeFile(
     path.join(__dirname, 'data/csv/reviews/reviewMetricsData.csv'),
@@ -86,16 +113,39 @@ const writeCSV = (amount) => {
       console.log('Wrote review summaries csv.');
     }
   );
+}
 
-   fs.writeFile(
-     path.join(__dirname, 'data/csv/reviews/reviewsOnly.csv'),
-     reviewsOnlyCSV,
-     (err) => {
-       if (err) throw err;
+const writeReviewsCSV = (last) => {
+  for (let i = last + 1; i <= last + batchSize; i++) {
+    let reviewData = combinedGenerator.createReviews(product_id, random);
 
-       console.log('Wrote only-reviews csv.');
-     }
-   );
-};
+    reviewsOnlyCSV += `${i},${
+      reviewData.reviews[z].submitter},${
+      reviewData.reviews[z].submission_date},${
+      reviewData.reviews[z].rating},${
+      reviewData.reviews[z].title},${
+      reviewData.reviews[z].text},${
+      reviewData.reviews[z].verified_purchase},${
+      reviewData.reviews[z].would_recommend},${
+      reviewData.reviews[z].helpful_count},${
+      reviewData.reviews[z].unhelpful_count},${
+      reviewData.reviews[z].rated_features},${
+      reviewData.reviews[z].quality_rating},${
+      reviewData.reviews[z].value_rating},${
+      reviewData.reviews[z].ease_of_use_rating}\n`;
 
-writeCSV(count);
+    random++;
+  }
+
+  fs.writeFile(
+    path.join(__dirname, 'data/csv/reviews/reviewsOnly.csv'),
+    reviewsOnlyCSV,
+    (err) => {
+      if (err) throw err;
+
+      console.log('Wrote only-reviews csv.');
+    }
+  );
+}
+
+// writeCSV(count);
